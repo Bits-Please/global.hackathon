6594a48f2daace365e5d0e2436b98c33c4c7be65
koding user <rayun.mehrab@mail.utoronto.ca>
2014-12-07 23:05:20 +0000

    Hashmap addition

--- Teams/Bits-Please/speech-recognizer.js
+++ Teams/Bits-Please/speech-recognizer.js
@@ -7,15 +7,15 @@
         } catch(e) {
             var recognition = Object;
         }
-        data_Hash = new HashData({1:"dog"});
+        data_Hash = new HashData({1:"dog", 2:"cat"});
         recognition.continuous = true;
         recognition.interimResults = true;
-        var currPic = 0; //@TODO: generate random number here for a random animal
+        var currPic = 2; //@TODO: generate random number here for a random animal
         var interimResult = '';
         var textArea = $('#speech-page-content');
         var textAreaID = 'speech-page-content';
 
-        $(eval('.' + data_Hash.getText(currPic) +'-mic')).click(function(){  //data_Hash is the object created form the Hasmap class
+        $(eval("'." + data_Hash.getText(currPic) +"-mic'")).click(function(){  //data_Hash is the object created form the Hasmap class
             startRecognition();
         });
 
@@ -34,12 +34,12 @@
     for (var i = event.resultIndex; i < event.results.length; ++i) {
         if (event.results[i].isFinal) {
             
-          if(event.results[i][0].transcript == "dog" || event.results[i][0].transcript == "dog " || event.results[i][0].transcript == " dog"){
+          if(event.results[i][0].transcript == data_Hash.getText(currPic) || event.results[i][0].transcript == (data_Hash.getText(currPic) + " ") || event.results[i][0].transcript == (" " + data_Hash.getText(currPic))){
                 insertAtCaret(textAreaID, "  CORRECT ");
                
            }else{
                insertAtCaret(textAreaID, "  FALSE ");
-                 var msg = new SpeechSynthesisUtterance('dog');
+                 var msg = new SpeechSynthesisUtterance(data_Hash.getText(currPic));
                  window.speechSynthesis.speak(msg);
            }
         }
